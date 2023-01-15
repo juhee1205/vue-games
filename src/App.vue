77086e095dfd5717a71b8a0d399085976cf00602
Joohee Kim <jooheekim@Jooheeui-MacBookAir.local>
2023-01-15 23:48:35 +0900

    first

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import LayoutHeader from "./components/Header";
+</script>
+<template>
+  <LayoutHeader />
+  <router-view />
+</template>
+
+<style lang="scss">
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <ul>
+      <li>카드 게임</li>
+      <li>가위바위보</li>
+      <li>숫자야구</li>
+    </ul>
+  </div>
+
+</template>
