f130de5fa1c597c47c2260406339e4f90a8c2b80
juhee <jhee_608@naver.com>
2023-02-22 23:05:12 +0900

    ♻️ [snake] 스코어추가 음식생성관련 함수 수정

--- src/views/Snake.vue
+++ src/views/Snake.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
+let CELL = 20
 let isPlay = ref<boolean>(true)
 let snake = ref<number[][]>([])
 let direction = ref<string>('Right')
 
-let CELL = 20
 let snakeLength: number = 0
 let head: number[] = [0,0]
+let [headX, headY]: number[] = head
 let bodyText: string[] = []
 let tail: number[] = [0,0]
+let score = ref<number>(0)
 
 const creatSnake  = (): void => {
-  for (let i = 0; i < 5; i++) {
+  snake.value = []
+  for (let i = 0; i < 5; i ++) {
     snake.value.push([i + ((CELL / 2) - 7), CELL / 2])
   }
 }
@@ -20,6 +23,7 @@ creatSnake()
 
 let food = ref<number[]>([])
 let showFood = ref<boolean>(false)
+let eatCount: number = 0
 
 const getRandomPosition = (): void => {
   let foodX: number = Math.floor(Math.random() * CELL)
@@ -33,32 +37,34 @@ const getRandomPosition = (): void => {
     snakeY.push(item[1])
   })
 
-  let xCheck = snakeX.indexOf(foodX)
-  let yCheck = snakeX.indexOf(foodY)
-  if (xCheck === -1 && yCheck === -1) {
+  let snakeXCheck = snakeX.indexOf(foodX)
+  let snakeYCheck = snakeY.indexOf(foodY)
+  if (snakeXCheck !== -1 && snakeYCheck !== -1) {
+    getRandomPosition()
+  } else {
     showFood.value = true
     food.value = [foodX, foodY]
-  } else {
-    getRandomPosition()
   }
 }
+
 const createFood = (): void => {
   getRandomPosition()
 }
+
 createFood()
 
-const isFood = () => {
+const isFood = (): void => {
   if (food.value.join(',') === head.join(',')) {
     snake.value.splice(0,0, [0,0])
     showFood.value = false
-    setTimeout(createFood, 2000)
+    eatCount ++
+    score.value += eatCount * 100
+    setTimeout(createFood, 1500)
   }
 }
 
 const isGameOver = (): boolean => {
   let headText: string = head.join(',')
-  let headX = head[0]
-  let headY = head[1]
 
   const right = direction.value === 'Right' && headX >= CELL - 1
   const left = direction.value === 'Left' && headX <= 0
@@ -68,24 +74,22 @@ const isGameOver = (): boolean => {
 
   if (bodyCross || right || left || up || down) {
     isPlay.value = false
-    console.log('게임종료')
     gameEnd()
     return false
   }
   return true
 }
 
-const moveSnake = () => {
-
+const moveSnake = (): void => {
   snakeLength = snake.value.length
   head = snake.value[snakeLength - 1]
+  headX = head[0]
+  headY = head[1]
   tail = snake.value[0]
   bodyText = snake.value.slice(0, snakeLength - 1).map(item => item.join(','))
-  let [headX, headY]: number[] = head
 
   if (isGameOver()) {
     isFood()
-
     snake.value.splice(0, 1)
 
     if (direction.value === 'Left') {
@@ -103,27 +107,28 @@ const moveSnake = () => {
   }
 }
 
-let paly = setInterval(() => {
-  moveSnake()
-}, 300)
+let interval: any = null
+const gameStart = (): void => {
+  interval = setInterval(moveSnake, 150)
+}
+gameStart()
 
-
-const gameEnd = () => {
-  clearInterval(paly)
+const gameEnd = (): void => {
+  clearInterval(interval)
 }
 
 const picePosition = ([x ,y]: number[]): string => {
   return `left: ${x * 30}px; top: ${y * 30}px`
 }
 
-const directionCheck = (dir) => {
+const directionCheck = (dir: string): string => {
   return dir === 'Left' || dir === 'Right'  ? 'row' : 'col'
 }
 
 window.addEventListener('keydown', (e: KeyboardEvent): void => {
   const beforeDirection = direction.value
 
-  if(e.code.includes('Arrow') && isPlay.value) {
+  if (e.code.includes('Arrow') && isPlay.value) {
     let keyDirection = e.code.split('Arrow')[1]
     if (directionCheck(direction.value) !== directionCheck(keyDirection)) {
       direction.value = keyDirection
@@ -131,8 +136,18 @@ window.addEventListener('keydown', (e: KeyboardEvent): void => {
   }
 });
 
-const isHead = (index) => {
+const isHead = (index: number): boolean => {
   return index === snake.value.length -1 ? true : false
+}
+
+const reStart = (): void => {
+  isPlay.value = true
+  direction.value = 'Right'
+  score.value = 0
+  eatCount = 0
+  creatSnake()
+  createFood()
+  gameStart()
 }
 
 </script>
@@ -142,10 +157,18 @@ const isHead = (index) => {
 
   <div class="gameBox">
 
+    <div class="reStart" v-if="!isPlay">
+      <h2>👻 게임 종료 👻</h2>
+      <div class="btn" @click="reStart()">
+        <font-awesome-icon icon="fa-solid fa-rotate-right" />
+        다시 시작
+      </div>
+    </div>
+
     <div class="ground">
       <div :class="['snake', direction]">
         <template v-for="(position, index) in snake" :key="`pice-${index}`">
-          <div class="pice" :style="picePosition(position)"><span :class="[{'head' : isHead(index) }]"><i></i></span></div>
+          <div class="pice" :style="picePosition(position)"><span :class="[{'head' : isHead(index) }, {'die': !isPlay}]"><i></i></span></div>
         </template>
       </div>
       <div class="food" :style="picePosition(food)" v-if="showFood">
@@ -154,6 +177,11 @@ const isHead = (index) => {
       <div class="gridBox" :style="`width: ${CELL * 30}px; height: ${CELL * 30}px `">
         <div class="grid" v-for="(box, index) in CELL*CELL" :key="`box-${index}`"></div>
       </div>
+    </div>
+
+    <div class="scoreZone">
+      <h2>Score</h2>
+      <p>{{ score }}</p>
     </div>
 
   </div>
